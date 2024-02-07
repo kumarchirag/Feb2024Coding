58a5500008e76d1e5b3ccefdf5e6ab422e78dfe2
kumarchirag <95746019+kumarchirag@users.noreply.github.com>
2024-02-07 20:40:12 +0530

    Update findMaxInRotatedSortedArray.java

--- findMaxInRotatedSortedArray.java
+++ findMaxInRotatedSortedArray.java
@@ -1,1 +1,28 @@
+class Solution {
+    public int findMin(int[] nums) {
+    
+        return nums[maxElement(nums)];
+        
+    }
 
+    static int maxElement(int[] arr){
+       int start = 0;
+        int end = arr.length-1;
+        
+        while(start < end){
+            int mid = start + (end-start)/2;
+            
+            if(mid<arr.length-1 && arr[mid]>arr[mid+1]){
+                return mid;
+            }
+            else if(arr[start] > arr[mid]){
+                end = mid-1;
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        return end;
+
+    }
+}
