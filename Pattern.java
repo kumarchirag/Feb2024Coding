bd1ed34f016823c2ccd3fbe1ca320fe0dbdcaf98
kumarchirag <95746019+kumarchirag@users.noreply.github.com>
2024-02-13 02:30:50 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        for(int i=4; i>=0; i--){
+            for(int j=i; j>=0; j--){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+        
+    }
+}
+* * * * * 
+* * * * 
+* * * 
+* * 
+*
