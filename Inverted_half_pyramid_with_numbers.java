6d2c0a34970e6df50ef0f4a2ba24a26025f94a80
Chirag Choudhary <95746019+kumarchirag@users.noreply.github.com>
2024-02-14 03:12:40 +0530

    Create Inverted_half_pyramid_with_numbers.java

---
+++ Inverted_half_pyramid_with_numbers.java
@@ -0,0 +1,21 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        int range =5;
+        for(int i=1; i<=5; i++){
+            for(int k=1; k<=range; k++){
+                System.out.print(k+" ");
+            }
+            range--;
+           System.out.println();
+        }
+    }
+}
+
+1 2 3 4 5 
+1 2 3 4 
+1 2 3 
+1 2 
+1 
