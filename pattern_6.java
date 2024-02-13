4a0cfe3b249a09ff93b2663dedd14e79197f289d
kumarchirag <95746019+kumarchirag@users.noreply.github.com>
2024-02-13 21:09:43 +0530

    Create pattern_6.java

---
+++ pattern_6.java
@@ -0,0 +1,20 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+
+class HelloWorld {
+    public static void main(String[] args) {
+        char c ='A';
+        for(int i=1; i<=4; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(c+" ");
+                c++;
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+A 
+B C 
+D E F 
+G H I J 
