c8ecfc28d73b3ef862b669b3699d4530628139ce
Chirag Choudhary <95746019+kumarchirag@users.noreply.github.com>
2024-02-14 04:47:01 +0530

    Create Solid_rohmbus.java

---
+++ Solid_rohmbus.java
@@ -0,0 +1,21 @@
+public class MyClass {
+    public static void main(String args[]) {
+       for(int i=1; i<=5; i++){
+            
+            for(int j=1; j<=5-i+1; j++){
+                System.out.print("  ");
+            }
+            
+            for(int j=1; j<=5; j++){
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
+
+          * * * * * 
+        * * * * * 
+      * * * * * 
+    * * * * * 
+  * * * * * 
