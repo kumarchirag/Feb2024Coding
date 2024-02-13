a3c2338cb79a7d3563a75b71b6f4c611508b009b
Chirag Choudhary <95746019+kumarchirag@users.noreply.github.com>
2024-02-14 04:51:36 +0530

    Update Solid_rohmbus.java

--- Solid_rohmbus.java
+++ Solid_rohmbus.java
@@ -2,7 +2,7 @@ public class MyClass {
     public static void main(String args[]) {
        for(int i=1; i<=5; i++){
             
-            for(int j=1; j<=5-i+1; j++){
+            for(int j=1; j<=5-i; j++){
                 System.out.print("  ");
             }
             
@@ -14,8 +14,8 @@ public class MyClass {
     }
 }
 
-          * * * * * 
         * * * * * 
       * * * * * 
     * * * * * 
   * * * * * 
+* * * * * 
