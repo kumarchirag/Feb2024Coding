a51421ac139469e10231c26fe7be2ac96f08cf59
Chirag Choudhary <95746019+kumarchirag@users.noreply.github.com>
2024-02-15 07:12:04 +0530

    Create Insertion_sort.java

---
+++ Insertion_sort.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void sortColors(int[] arr) {
+         int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        
+    }
+}
+
+// Example 1:
+
+// Input: nums = [2,0,2,1,1,0]
+// Output: [0,0,1,1,2,2]
+// Example 2:
+
+// Input: nums = [2,0,1]
+// Output: [0,1,2]
